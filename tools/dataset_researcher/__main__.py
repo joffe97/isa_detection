07fe382e65c4ae5761e7be4eb75678329815a420
Joachim Andreassen <joachima94@gmail.com>
2024-06-12 16:25:42 +0200

    update

--- tools/dataset_researcher/__main__.py
+++ tools/dataset_researcher/__main__.py
@@ -9,7 +9,11 @@ sys.path.insert(1, "/home/joachan/isa_detection/src")
 sys.path.insert(1, "/home/joachan/isa_detection/tools/dataset_researcher")
 
 from config import Config
-from domains.feature.bytes_computers import FourierComputer, AutoCorrelationComputer
+from domains.feature.bytes_computers import (
+    FourierComputer,
+    AutoCorrelationComputer,
+    AutoCorrelationFftComputer,
+)
 from domains.dataset import IsaDetect, CpuRec, IsaDetectCode
 from domains.dataset.custom.sinus_signal import SinusSignal, CustomDataset
 from researchers import (
@@ -28,7 +32,9 @@ def my_autocorr(x):
     return result[result.size // 2 :]
 
 
-def autocorr_plotfunc(x, lag_range: Optional[tuple[int, int]] = None) -> np.ndarray:
+def autocorr_plotfunc(
+    x, lag_range: Optional[tuple[int, int]] = None
+) -> np.ndarray:
     AUTOCORR_LINE_INDEX = 5
 
     fig, ax = plt.subplots()
@@ -36,17 +42,21 @@ def autocorr_plotfunc(x, lag_range: Optional[tuple[int, int]] = None) -> np.ndar
     [_, y] = ax.lines[AUTOCORR_LINE_INDEX].get_data()
     plt.close(fig)
 
-    return y[lag_range[0] - 1 : min(lag_range[1] - 1, len(y))] if lag_range is not None else y
+    return (
+        y[lag_range[0] - 1 : min(lag_range[1] - 1, len(y))]
+        if lag_range is not None
+        else y
+    )
 
 
 if __name__ == "__main__":
     MAX_READ = 64000
     # MAX_READ = 1200
+    # MAX_READ = 10
     # MAX_READ = 20000
-    lag_max = 16
     # datasets = [CpuRec(), IsaDetectCode(100)]
     # datasets = [IsaDetectCode(5), CpuRec()]
-    datasets = [CpuRec()]
+    datasets = [CpuRec(), IsaDetectCode(1), IsaDetect(1)]
     # datasets = [IsaDetectCode(5)]
     # datasets = [SinusSignal()]
     for dataset in datasets:
@@ -55,6 +65,9 @@ if __name__ == "__main__":
         #     autocorr_times=1,
         #     max_data_len_for_higher_autocorr=1000,
         # )
-        bytes_computer = FourierComputer(32)
-        BytesComputerPlotter(MAX_READ, bytes_computer).research(dataset)
-        AutoCorrelationMeanPeakTable(MAX_READ, lag_max, lag_min=2).research(dataset)
+        # bytes_computer = FourierComputer(32)
+        # bytes_computer = AutoCorrelationFftComputer(20)
+        # bytes_computer = AutoCorrelationFftComputer(16)
+        # bytes_computer = FourierComputer(16)
+        # BytesComputerPlotter(MAX_READ, bytes_computer).research(dataset)
+        AutoCorrelationMeanPeakTable(MAX_READ, 32, lag_min=2).research(dataset)
--- tools/dataset_researcher/researchers/auto_correlation_mean_peak_table.py
+++ tools/dataset_researcher/researchers/auto_correlation_mean_peak_table.py
@@ -8,20 +8,24 @@ from .researcher import Researcher
 
 
 class AutoCorrelationMeanPeakTable(Researcher):
-    def __init__(self, byte_read_count: int, lag_max: int, lag_min: int = 1) -> None:
+    def __init__(
+        self, byte_read_count: int, lag_max: int, lag_min: int = 1
+    ) -> None:
         self.byte_read_count = byte_read_count
         self.lag_max = lag_max
         self.lag_min = lag_min
 
     def research(self, dataset: BinaryFileDataset):
         autocorr_computer = AutoCorrelationComputer(self.lag_max, self.lag_min)
-        architecture_file_datas_mapping = dataset.create_architecture_func_data_mapping(
-            self.byte_read_count, autocorr_computer.compute
+        architecture_file_datas_mapping = (
+            dataset.create_architecture_func_data_mapping(
+                self.byte_read_count, autocorr_computer.compute
+            )
         )
         mean_datas = architecture_file_datas_mapping.mean_datas()
 
         labels = CorpusLabels(
-            included_labels={LabelEntry.INSTRUCTION_SIZE}
+            included_labels={LabelEntry.FIXED_INSTRUCTION_SIZE}
         ).load_as_architecture_labels_mapping()
         architecture_data_mapping: dict[str, list[str]] = dict()
         columns: list[str] = [
@@ -31,30 +35,68 @@ class AutoCorrelationMeanPeakTable(Researcher):
                 for i in range(1, 4)
             ),
             LabelEntry.INSTRUCTION_SIZE.value,
+            "match_index",
         ]
-        for architecture, mean_data in sorted(mean_datas.items(), key=lambda item: item[0].lower()):
+        for architecture, mean_data in sorted(
+            mean_datas.items(), key=lambda item: item[0].lower()
+        ):
             mean_data_sorted_by_value = sorted(
                 enumerate(mean_data, autocorr_computer.lag_min),
                 key=lambda enumerated_value: enumerated_value[1],
                 reverse=True,
             )
-            mean_data_peaks = [str(mean_data_peak) for mean_data_peak, _ in mean_data_sorted_by_value[:3]]
-            mean_data_peak_bits = [
-                str(mean_data_peak * 8) for mean_data_peak, _ in mean_data_sorted_by_value[:3]
+            mean_data_peaks = [
+                str(mean_data_peak)
+                for mean_data_peak, _ in mean_data_sorted_by_value[:3]
             ]
-            instruction_size = str(labels.get(architecture, dict()).get(LabelEntry.INSTRUCTION_SIZE))
+            mean_data_peak_bits = [
+                str(mean_data_peak * 8)
+                for mean_data_peak, _ in mean_data_sorted_by_value[:3]
+            ]
+            instruction_size = labels.get(architecture, dict()).get(
+                LabelEntry.FIXED_INSTRUCTION_SIZE
+            )
+            if instruction_size is None:
+                continue
+
+            instruction_size_str = str(instruction_size)
+            match_index = (
+                str(mean_data_peak_bits.index(instruction_size_str))
+                if instruction_size_str in mean_data_peak_bits
+                else "-"
+            )
             architecture_data_mapping[architecture] = [
                 *mean_data_peaks,
                 *mean_data_peak_bits,
-                instruction_size,
+                instruction_size_str,
+                match_index,
             ]
 
-        dataframe = pandas.DataFrame.from_dict(architecture_data_mapping, orient="index", columns=columns)
+        dataframe = pandas.DataFrame.from_dict(
+            architecture_data_mapping, orient="index", columns=columns
+        )
         pandas.set_option("display.max_rows", dataframe.shape[0] + 1)
 
-        group_name = "_".join(map(str, [self.byte_read_count, f"{self.lag_min}-{self.lag_max}"]))
+        match_index_counts = dataframe["match_index"].value_counts()
+        match_index_frequencies = dict(
+            (
+                match_str,
+                match_index_counts.get(match_str, 0)
+                / len(dataframe["match_index"]),
+            )
+            for match_str in ["0", "1", "2", "-"]
+        )
+
+        match_index_frequencies_str = "\n".join(
+            f"Match index {key}: {value}"
+            for key, value in match_index_frequencies.items()
+        )
+
+        group_name = "_".join(
+            map(str, [self.byte_read_count, f"{self.lag_min}-{self.lag_max}"])
+        )
         file_name = dataset.identifier()
         path = self._create_result_path(group_name, file_name, ".txt")
         path.parent.mkdir(parents=True, exist_ok=True)
         with open(path, "w") as f:
-            f.write(str(dataframe))
+            f.write(f"{str(dataframe)}\n\n{match_index_frequencies_str}")
